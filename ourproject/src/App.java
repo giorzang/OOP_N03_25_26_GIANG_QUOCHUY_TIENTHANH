b8a473ff1721a027e347bf1dca646aa1ab1ad547
Vũ Trường Giang <84860408+giorzang@users.noreply.github.com>
2025-08-21 07:47:06 +0000

    new file:   ourproject/.vscode/settings.json new file:   ourproject/README.md new file:   ourproject/bin/App.class new file:   ourproject/src/App.java

---
+++ ourproject/src/App.java
@@ -0,0 +1,7 @@
+public class App {
+
+    // Main: trong mot Project chi co mot Main la dong bo
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}

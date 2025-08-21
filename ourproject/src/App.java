166c8043109cdf5fa652532ed81c57ee76e2336c
Vũ Trường Giang <84860408+giorzang@users.noreply.github.com>
2025-08-21 07:52:41 +0000

    New commit

--- ourproject/src/App.java
+++ ourproject/src/App.java
@@ -2,6 +2,6 @@ public class App {
 
     // Main: trong mot Project chi co mot Main la dong bo
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Try a new commit");
     }
 }

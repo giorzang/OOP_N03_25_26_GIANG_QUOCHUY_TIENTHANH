ebbf41f086ef145ba321e5cc93db027863c4cc7e
Thank-05 <thanhtiennguyen4805@gmail.com>
2025-08-22 11:10:27 +0000

    Nu

---
+++ ourproject/src/Model/Nu.java
@@ -0,0 +1,23 @@
+package Model;
+
+public class Nu {
+
+public class Nam {
+    //thuoc tinh
+    public String Name;
+    public String Major;
+    public String Born;
+    public String DateofBirth;
+
+    //phuong thuoc
+    public void print(String name, String major, String born, String dob){
+        Name = name;
+        Major = major;
+        Born = born;
+        DateofBirth = dob;
+
+        System.out.println("Full name : " + Name + "Major: " + Major + " Place of Birth : " + born + "Date of Birth " + dob);
+    }
+}
+
+}

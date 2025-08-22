eabca35e4ec9d5747aa6ea1417705f2f0c03be08
Thank-05 <thanhtiennguyen4805@gmail.com>
2025-08-22 11:04:37 +0000

    update Nam

--- ourproject/src/App.java
+++ ourproject/src/App.java
@@ -3,5 +3,10 @@ public class App {
     // Main: trong mot Project chi co mot Main la dong bo
     public static void main(String[] args) throws Exception {
         System.out.println("Try a new commit");
+
+
+
+
+        
     }
 }

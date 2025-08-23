31d51b9c53e31e4661de628b8adf42cb27543ca4
Thank-05 <thanhtiennguyen4805@gmail.com>
2025-08-23 17:52:43 +0700

    Update testUniversities.java

--- ourproject/src/Test/testUniversities.java
+++ ourproject/src/Test/testUniversities.java
@@ -1,3 +1,11 @@
-public class Major{
-    
+public class TestUniversities {
+    public static void main(String[] args) {
+        // Tao doi tuong truong dai hoc
+        Universities u1 = new Universities();
+        Universities u2 = new Universities();
+
+        // Goi ham print de nhap thong tin va in ra
+        u1.print("U001", "Dai hoc Bach Khoa", "Ha Noi");
+        u2.print("U002", "Dai hoc Kinh Te", "TP. HCM");
+    }
 }

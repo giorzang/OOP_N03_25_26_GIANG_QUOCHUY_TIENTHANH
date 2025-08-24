b6a094c904926deeafd002992bc1070b39d2da77
Thank-05 <thanhtiennguyen4805@gmail.com>
2025-08-24 14:10:43 +0700

    Update Facilities.java

--- ourproject/src/Model/Facilities.java
+++ ourproject/src/Model/Facilities.java
@@ -1,15 +1,21 @@
-public class Facilities{
+public class Facilities {
+    // cac thuoc tinh
+    public String chisoGiaoVien;
+    public int soLuongPhongHoc;
+    public int soLuongLab;
+    public String chatLuongPhongHoc;
+    public String chatLuongGiaoVien;
+    public String heThongQuanLy;
+    public String chiSoTrangThietBiTheThao;
 
-    public chisoGiaoVien
-    public soLuongPhongHoc
-    public soLuongLab
-    public chatLuongPhongHoc
-    public chatLuongGiaoVien
-    public heThongQuanLy
-    public chiSoTrangThietBitheThao
-
-    //phuong thuc
-    public void print(String chisogiaovien, String soluongphonghoc, String soluonglab, String chatluongphonghoc, String chatluonggiaovien, String hethongquanly, String chisotrangthietbithethao) {
-        System.out.println("Chi so GV: " + chisoGiaoVien + "So luong phong hoc: " + soLuongPhongHoc + "So luong lab: " + soLuongLab + "Chat luong phong hoc: " + chatLuongPhongHoc + "He thong quan ly: " + heThongQuanLy + "Chi so trang thiet bi the thao");
+    // phuong thuc in thong tin
+    public void print() {
+        System.out.println("Chi so GV: " + chisoGiaoVien);
+        System.out.println("So luong phong hoc: " + soLuongPhongHoc);
+        System.out.println("So luong lab: " + soLuongLab);
+        System.out.println("Chat luong phong hoc: " + chatLuongPhongHoc);
+        System.out.println("Chat luong giao vien: " + chatLuongGiaoVien);
+        System.out.println("He thong quan ly: " + heThongQuanLy);
+        System.out.println("Chi so trang thiet bi the thao: " + chiSoTrangThietBiTheThao);
     }
 }

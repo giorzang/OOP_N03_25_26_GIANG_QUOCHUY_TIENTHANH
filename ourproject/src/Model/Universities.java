937e913fe5f028b74028029653180a7e15570895
Thank-05 <thanhtiennguyen4805@gmail.com>
2025-08-24 14:11:38 +0700

    Update Universities.java

--- ourproject/src/Model/Universities.java
+++ ourproject/src/Model/Universities.java
@@ -1,14 +1,16 @@
 public class Universities {
     public String maTruong;
-    public String tenTruong
+    public String tenTruong;
     public String diaDiem;
 
-    //phuong thuc
+    // phuong thuc
     public void print(String ma, String ten, String diadiem) {
         maTruong = ma;
         tenTruong = ten;
         diaDiem = diadiem;
 
-        System.out.println("Ma truong: " + maTruong + "Ten truong: " + tenTruong + "Dia diam: " + diaDiem);
+        System.out.println("Ma truong: " + maTruong);
+        System.out.println("Ten truong: " + tenTruong);
+        System.out.println("Dia diem: " + diaDiem);
     }
 }

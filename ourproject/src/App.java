34128127cf8ead1ca3c7678718a6a0bfcc700249
Thank-05 <thanhtiennguyen4805@gmail.com>
2025-08-24 14:13:13 +0700

    Update App.java

--- ourproject/src/App.java
+++ ourproject/src/App.java
@@ -1,12 +1,17 @@
 public class App {
+    public static void main(String[] args) {
+        Facilities f = new Facilities();
 
-    // Main: trong mot Project chi co mot Main la dong bo
-    public static void main(String[] args) throws Exception {
-        System.out.println("Try a new commit");
+        // gan gia tri
+        f.chisoGiaoVien = "Tot";
+        f.soLuongPhongHoc = 20;
+        f.soLuongLab = 5;
+        f.chatLuongPhongHoc = "Kha";
+        f.chatLuongGiaoVien = "Tot";
+        f.heThongQuanLy = "Hien dai";
+        f.chiSoTrangThietBiTheThao = "Day du";
 
-
-
-
-        
+        // in ra
+        f.print();
     }
 }

6475ebaa7c67a726859670ada2dc501a237add04
Thank-05 <thanhtiennguyen4805@gmail.com>
2025-08-28 13:46:43 +0000

    Update Week 2

---
+++ ourproject/src/Model/randNum.java
@@ -0,0 +1,8 @@
+package Model;
+import java.util.Random;
+
+public class randNum {
+    public static int ranNumm() {
+      return new Random().nextInt(1000);    
+    }  
+}
---
+++ ourproject/src/Test/TestRandNum.java
@@ -0,0 +1,8 @@
+import Model.randNum;
+
+public class TestRandNum {
+    public static void main (String[] agrs){
+        int randomnumber = randNum.ranNumm();
+        System.out.println("RandomofNumber:"+randomnumber);
+    }
+}

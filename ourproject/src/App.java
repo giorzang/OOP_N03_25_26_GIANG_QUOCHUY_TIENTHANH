6de16222bca5cb16dec636d12ccd4badcecf584b
Thank-05 <thanhtiennguyen4805@gmail.com>
2025-08-28 13:11:16 +0000

    Update BT1 Lecture 2: Program Control

--- ourproject/src/App.java
+++ ourproject/src/App.java
@@ -1,17 +1,5 @@
 public class App {
     public static void main(String[] args) {
-        Facilities f = new Facilities();
-
-        // gan gia tri
-        f.chisoGiaoVien = "Tot";
-        f.soLuongPhongHoc = 20;
-        f.soLuongLab = 5;
-        f.chatLuongPhongHoc = "Kha";
-        f.chatLuongGiaoVien = "Tot";
-        f.heThongQuanLy = "Hien dai";
-        f.chiSoTrangThietBiTheThao = "Day du";
-
-        // in ra
-        f.print();
+        
     }
 }

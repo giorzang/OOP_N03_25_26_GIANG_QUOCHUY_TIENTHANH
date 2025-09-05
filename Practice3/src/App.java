b4714aff67eec1fc6f9ffdb6558a7d0d3915150a
Vũ Trường Giang <84860408+giorzang@users.noreply.github.com>
2025-09-05 10:24:57 +0000

    Update classes User, TestUser

--- Practice3/src/App.java
+++ Practice3/src/App.java
@@ -1,5 +1,7 @@
+//import test.TestUser;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        TestUser.test();
     }
 }
---
+++ Practice3/test/TestUser.java
@@ -0,0 +1,8 @@
+//import User;
+
+public class TestUser {
+    public static void test() {
+        User u = new User("user123", "123*****");
+        System.out.println("User: " + u.getUsername() + ", password: " + u.getPassword());
+    }
+}

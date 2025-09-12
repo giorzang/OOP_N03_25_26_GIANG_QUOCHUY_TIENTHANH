e5b4b21ba162146080828e74db352d7a048ca4e6
Vũ Trường Giang <84860408+giorzang@users.noreply.github.com>
2025-09-12 18:18:43 +0000

    Tested class User

--- FinalProject/src/App.java
+++ FinalProject/src/App.java
@@ -1,5 +1,17 @@
+import model.User;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("\nTest Get:");
+        TestUser.testGet();
+
+        System.out.println("\nTest Set:");
+        TestUser.testSet();
+
+        System.out.println("\nTest Login:");
+        TestUser.testLogin();
+
+        System.out.println("\nTest Update:");
+        TestUser.testUpdate();
     }
 }
---
+++ FinalProject/test/TestUser.java
@@ -0,0 +1,48 @@
+import model.User;
+
+public class TestUser {
+    public static void testGet() {
+        User u = new User(1, "Giang", "giorzang@gmail.com", "pass123", "0818778269");
+        
+        System.out.println("id = " + u.getId());
+        System.out.println("name = " + u.getName());
+        System.out.println("email = " + u.getEmail());
+        System.out.println("password = " + u.getPassword());
+        System.out.println("phone = " + u.getPhone());
+    }
+
+    public static void testSet() {
+        User u = new User(1, "Giang", "giorzang@gmail.com", "pass123", "0818778269");
+
+        u.setId(2);
+        u.setName("Thanh");
+        u.setEmail("Thank-05@gmail.com");
+        u.setPassword("pass321");
+        u.setPhone("0123456789");        
+
+        System.out.println("id = " + u.getId());
+        System.out.println("name = " + u.getName());
+        System.out.println("email = " + u.getEmail());
+        System.out.println("password = " + u.getPassword());
+        System.out.println("phone = " + u.getPhone());
+    }
+    
+    public static void testLogin() {
+        User u = new User(1, "Giang", "giorzang@gmail.com", "pass123", "0818778269");
+
+        System.out.println("(" + u.login("giorzang@gmail.com", "pass321") + ") Sai email hoac mat khau.");
+        System.out.println("(" + u.login("giorzang@gmail.com", "pass123") + ") Dang nhap thanh cong!");
+    }
+
+    public static void testUpdate() {
+        User u = new User(1, "Giang", "giorzang@gmail.com", "pass123", "0818778269");
+
+        u.update("Thanh", "Thank-05@gmail.com", "pass321", "0123456789");
+
+        System.out.println("id = " + u.getId());
+        System.out.println("name = " + u.getName());
+        System.out.println("email = " + u.getEmail());
+        System.out.println("password = " + u.getPassword());
+        System.out.println("phone = " + u.getPhone());
+    }
+}

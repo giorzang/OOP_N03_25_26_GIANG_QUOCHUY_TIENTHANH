c09f88942e6e0319769d584ac2cf5bd80690870a
Thank-05 <thanhtiennguyen4805@gmail.com>
2025-10-09 08:33:57 +0000

    update

--- Web/complete/src/main/java/com/example/servingwebcontent/repositories/ProductRepository.java
+++ Web/complete/src/main/java/com/example/servingwebcontent/repositories/ProductRepository.java
@@ -6,7 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
-    // Spring Data JPA sẽ tự động cung cấp các phương thức CRUD cơ bản.
-    // Chúng ta có thể thêm các phương thức truy vấn tùy chỉnh ở đây sau này.
-    // Ví dụ: List<Product> findByNameContaining(String name);
+
 }
--- Web/complete/src/main/java/com/example/servingwebcontent/repositories/UserRepository.java
+++ Web/complete/src/main/java/com/example/servingwebcontent/repositories/UserRepository.java
@@ -8,6 +8,6 @@ import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    // Thêm phương thức tìm user bằng username để sau này làm chức năng đăng nhập
+
     Optional<User> findByUsername(String username);
 }

738c9f11949de558f657cebfca00daaf61e328e3
Thank-05 <thanhtiennguyen4805@gmail.com>
2025-10-21 14:52:08 +0000

    update 10/21

---
+++ Web/complete/src/main/java/com/example/servingwebcontent/database/aivenConnection.java
@@ -0,0 +1,41 @@
+package com.example.servingwebcontent.Database;
+
+import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Locale;
+
+public class aivenConnection {
+ public void aivenConn() {
+        Connection conn = null;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            conn = DriverManager.getConnection(
+                    "jdbc:mysql:/mysql://avnadmin:AVNS_YrDmXKzkfpb9jqmSUvv@mysql-rionguyen05-thanhtiennguyen4805-b46f.b.aivencloud.com:28205/defaultdb?ssl-mode=REQUIRED",
+                    "sqluser", "password");
+
+               
+            Statement sta = conn.createStatement();
+            ResultSet reset = sta.executeQuery("select * from user");
+            System.out.println("Display data from database: ");
+            while (reset.next()) {
+                String userID = reset.getString("userId");
+                String username = reset.getString("username");
+                String address = reset.getString("address");
+                System.out.println(userID + " " + username + " " + address);
+
+            }
+
+            reset.close();
+            sta.close();
+            conn.close();
+        } catch (Exception e) {
+            System.out.println("Error in database connecion");
+            System.out.println(e);
+            e.printStackTrace();
+        }
+    }
+}
